c3dbe90bf2dc45aa48bad82c7d1bfa707ca913d1
mowebiaz <mowebiaz@gmail.com>
2025-06-20 16:05:24 +0200

    Add sanity webhook and route for revalidate path

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,20 @@
-import { sanityFetch } from "@/src/sanity/live";
-import { POSTS_QUERY } from "@/src/sanity/queries";
+import { sanityFetch } from '@/src/sanity/live'
+import { POSTS_QUERY } from '@/src/sanity/queries'
+
 
 export default async function Home() {
-const { data: posts } = await sanityFetch({ query: POSTS_QUERY });
-
+  const { data: posts } = await sanityFetch({ query: POSTS_QUERY })
 
   return (
     <main>
-        {posts.map((post) => (
-          <div key={post._id}>
-            {post.title}
-            {post.slug && (
-              <a href={`/posts/${post.slug.current}`}>
-                clic
-              </a>
-            )}
-{/*             {post.body && <PortableText value={post.body} />}
- */}            
- </div> 
-
-        ))}
+      {posts.map((post) => (
+        <div key={post._id}>
+          {post.title}
+          {post.slug && <a href={`/posts/${post.slug.current}`}>clic</a>}
+          {/*             {post.body && <PortableText value={post.body} />}
+           */}
+        </div>
+      ))}
     </main>
-  );
+  )
 }
