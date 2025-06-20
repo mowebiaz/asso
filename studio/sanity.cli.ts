467001a0290863ff63d8fadc292b28f9589bdada
mowebiaz <mowebiaz@gmail.com>
2025-06-20 11:52:55 +0200

    Add studioHost variable

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: '87m92kyy',
     dataset: 'production'
   },
+    studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to learn more about using environment variables for local & production.
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
